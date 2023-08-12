739e81d5eb0ee8e6dd35b5d8ee58d31a34a6ebb3
@vadim17 <vadim@genesort.com>
2023-08-12 15:15:17 +0300

    commit

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,13 +1,16 @@
-import {FC, useState} from 'react';
+// import {useState} from 'react';
 import './modal.scss';
 import cn from 'classnames';
+import {ReactElement} from 'react';
 
-const Modal = ({children, mode, onClick}: any):JSX.Element => {
-  const [count, setCount] = useState(0);
+const Modal = ({children, mode, onClick}: never): ReactElement => {
+  // const [count, setCount] = useState(0);
 
   return (
-    <div className={cn('modal-wrapper', {'open': mode})} onClick={onClick}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>{children}</div>
+    <div className={cn('modal-wrapper', {open: mode})} onClick={onClick}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
+        {children}
+      </div>
     </div>
   );
 };
