fb9d614d5f66885a136f6b54649d22a8630c0b9c
@vadim17 <vadim@genesort.com>
2023-08-18 13:01:04 +0300

    commit

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,14 +1,14 @@
 // import {useState} from 'react';
 import './modal.scss';
 import cn from 'classnames';
-import {ReactElement} from 'react';
+import { ReactElement } from 'react';
 
-const Modal = ({children, mode, onClick}: never): ReactElement => {
+const Modal = ({ children, mode, onClick }: never): ReactElement => {
   // const [count, setCount] = useState(0);
 
   return (
-    <div className={cn('modal-wrapper', {open: mode})} onClick={onClick}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
+    <div className={cn('modal-wrapper', { open: mode })} onClick={onClick}>
+      <div className='modal-content' onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './app';
-import {Provider} from 'react-redux';
-import {store} from './store/store';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
 );
