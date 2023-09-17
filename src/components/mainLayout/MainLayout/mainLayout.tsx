a2d421dba1a341ab74e256322d073e2a7ced5490
@vadim17 <vadim@genesort.com>
2023-09-17 14:20:25 +0300

    commit

--- src/components/mainLayout/MainLayout/mainLayout.tsx
+++ src/components/mainLayout/MainLayout/mainLayout.tsx
@@ -1,27 +1,36 @@
 import React, { FC } from 'react';
 import { Link, Outlet, useNavigation } from 'react-router-dom';
+import './mainLayout.scss';
 
 export type MainLayoutProps = NonNullable<unknown>;
 
 export const MainLayout: FC<MainLayoutProps> = () => {
   const navigation = useNavigation();
   return (
-    <div>
-      <h1>Darts</h1>
-      <div style={{ position: 'fixed', top: 0, right: 0 }}>
-        {navigation.state !== 'idle' && <p>Navigation in progress...</p>}
+    <div className='main-layout-wrapper'>
+      <div className='header'>
+        <div className='logo-side'>
+          <h1>Darts</h1>
+        </div>
+        <div className='nav-side'>
+          {navigation.state !== 'idle' ? (
+            <div style={{ position: 'fixed', top: 0, right: 0 }}>
+              <p>Navigation in progress...</p>
+            </div>
+          ) : (
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/exercise'>Exercise</Link>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
       </div>
-
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/exercise'>Exercise</Link>
-          </li>
-        </ul>
-      </nav>
 
       <hr />
 
